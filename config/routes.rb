1c4c7987b7a958365dadbdf4fbfa2066df65d19a
Chris Martine <cmmartine@gmail.com>
2023-02-13 16:55:06 -0500

    Added confirmation/welcome email sending to users upon signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   put '/request/:id/send', to: 'users#send_request', as: 'send_request'
   put '/request/:id/accept', to: 'users#accept_request', as: 'accept_request'
   delete 'request/:id/deny', to: 'users#deny_request', as: 'deny_request'
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
