6d34a73f08777d650c306f20512bbecbc8b87f3e
Chris Martine <cmmartine@gmail.com>
2023-02-22 16:49:56 -0500

    Added mailer setup to production env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,7 @@ Rails.application.configure do
 
   # Devise setup
   config.action_mailer.default_url_options = { host: 'cool-voice-7530.fly.dev' }
-
-  # letter_opener
+  
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'cool-voice-7530.fly.dev' }
+
+  # letter_opener
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDMAIL_USERNAME'],
+    password: ENV['SENDMAIL_PASSWORD'],
+    domain: 'cool-voice-7530.fly.dev',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -91,3 +108,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options[:host] = 'cool-voice-7530.fly.dev'
